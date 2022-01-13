2e362234ca2289642da78ae1afe352c059129daa
Анастасия <tolk-nastya@mai.ru>
2022-01-13 18:19:35 +0300

    simple calculator

---
+++ main.py
@@ -0,0 +1,11 @@
+a = float(input("Введите  первое число:"))
+b = float(input("Ведите  второе число:"))
+c = input("Введите операцию:")
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
